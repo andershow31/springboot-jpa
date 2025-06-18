aa7dd32b7a8dcf9075838a3d724ea76adc11f73c
Anderson Pires <148875704+andershow31@users.noreply.github.com>
2025-06-18 15:03:55 -0300

    OrderStatus enum

--- course/src/main/java/com/projetoAnderShow/course/entities/Order.java
+++ course/src/main/java/com/projetoAnderShow/course/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.projetoAnderShow.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -30,15 +31,17 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	private Integer orderStatus;
 	
 	public Order() {
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
 		this.id = id;
 		this.moment = moment;
 		this.client = client;
+		this.setOrderStatus(orderStatus);
 	}
 
 	public Long getId() {
@@ -64,6 +67,20 @@ public class Order implements Serializable{
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		
+		if(orderStatus!=null) {
+
+			this.orderStatus = orderStatus.getCode();
+		}
+		
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -81,7 +98,8 @@ public class Order implements Serializable{
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
+
 	
 	
 }
